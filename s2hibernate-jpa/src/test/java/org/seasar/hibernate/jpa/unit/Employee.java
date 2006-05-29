ad09df731ec48d27839199f74e0ecb1f1d2244e3
nakamura-to <toshihiro.nakamura@gmail.com>
2006-05-29 17:01:34 +0000

    HibernateProxiedObjectResolverを追加。
    git-svn-id: https://www.seasar.org/svn/s2hibernate/trunk@67 d0420757-6e02-0410-97e1-b335d905b555

---
+++ s2hibernate-jpa/src/test/java/org/seasar/hibernate/jpa/unit/Employee.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.hibernate.jpa.unit;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "EMP2")
+public class Employee {
+
+	private int empno;
+
+	private String ename;
+
+	@Id
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public boolean equals(Object other) {
+		if (!(other instanceof Employee))
+			return false;
+		Employee castOther = (Employee) other;
+		return this.getEmpno() == castOther.getEmpno();
+	}
+
+	public int hashCode() {
+		return (int) this.getEmpno();
+	}
+}
