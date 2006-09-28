40778988625271815896c3d1bc1b2dd2741e5d72
Koichi Kobayashi <koichik@improvement.jp>
2006-09-28 06:42:26 +0000

    r:111 でコミットしそこねていた．心より恥じる 移動したクラスと同名のインタフェースを追加したのだが SVN 的には移動と認識されなくなってしまったらしい．
    git-svn-id: https://www.seasar.org/svn/s2hibernate/trunk@112 d0420757-6e02-0410-97e1-b335d905b555

--- s2hibernate-jpa/src/main/java/org/seasar/hibernate/jpa/S2HibernateConfiguration.java
+++ s2hibernate-jpa/src/main/java/org/seasar/hibernate/jpa/S2HibernateConfiguration.java
@@ -16,160 +16,38 @@
 package org.seasar.hibernate.jpa;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-
-import org.seasar.framework.autodetector.ClassAutoDetector;
-import org.seasar.framework.autodetector.ResourceAutoDetector;
-import org.seasar.framework.log.Logger;
-import org.seasar.framework.util.tiger.CollectionsUtil;
 
 /**
- * 
  * @author taedium
  */
-public class S2HibernateConfiguration {
+public interface S2HibernateConfiguration {
 
-    protected static final Logger logger = Logger
-            .getLogger(S2HibernateConfiguration.class);
+    void addMappingFile(final String fileName);
 
-    protected boolean autoDetection;
+    void addMappingFile(final String unitName, final String fileName);
 
-    protected Map<String, List<String>> mappingFiles = CollectionsUtil
-            .newHashMap();
+    void addMappingFileStream(final InputStream inputStream);
 
-    protected Map<String, List<InputStream>> mappingFileStreams = CollectionsUtil
-            .newHashMap();
+    void addMappingFileStream(final String unitName,
+            final InputStream inputStream);
 
-    protected Map<String, List<Class<?>>> persistenceClasses = CollectionsUtil
-            .newHashMap();
+    void addPersistenceClass(final Class<?> clazz);
 
-    public void addMappingFile(final String fileName) {
-        addMappingFile(null, fileName);
-    }
+    void addPersistenceClass(final String unitName, final Class<?> clazz);
 
-    public void addMappingFile(final String unitName, final String fileName) {
-        if (!mappingFiles.containsKey(unitName)) {
-            mappingFiles.put(unitName, new ArrayList<String>());
-        }
-        mappingFiles.get(unitName).add(fileName);
-    }
+    List<String> getMappingFiles();
 
-    public void addMappingFileStream(final InputStream inputStream) {
-        addMappingFileStream(null, inputStream);
-    }
+    List<String> getMappingFiles(final String unitName);
 
-    public void addMappingFileStream(final String unitName,
-            final InputStream inputStream) {
-        if (!mappingFileStreams.containsKey(unitName)) {
-            mappingFileStreams.put(unitName, new ArrayList<InputStream>());
-        }
-        mappingFileStreams.get(unitName).add(inputStream);
-    }
+    List<InputStream> getMappingFileStreams();
 
-    public void addPersistenceClass(final Class<?> clazz) {
-        addPersistenceClass(null, clazz);
-    }
+    List<InputStream> getMappingFileStreams(final String unitName);
 
-    public void addPersistenceClass(final String unitName, final Class<?> clazz) {
-        if (!persistenceClasses.containsKey(unitName)) {
-            persistenceClasses.put(unitName, new ArrayList<Class<?>>());
-        }
-        persistenceClasses.get(unitName).add(clazz);
-    }
+    List<Class<?>> getPersistenceClasses();
 
-    public void setMappingFileAutoDetector(
-            final ResourceAutoDetector[] detectors) {
-        for (final ResourceAutoDetector detector : detectors) {
-            addMappingFileAutoDetector(detector);
-        }
-    }
+    List<Class<?>> getPersistenceClasses(final String unitName);
 
-    public void addMappingFileAutoDetector(final ResourceAutoDetector detector) {
-        addMappingFileAutoDetector(null, detector);
-    }
+    boolean isAutoDetection();
 
-    public void addMappingFileAutoDetector(final String unitName,
-            final ResourceAutoDetector detector) {
-        autoDetection = true;
-
-        for (final ResourceAutoDetector.Entry entry : detector.detect()) {
-            if (logger.isDebugEnabled()) {
-                if (unitName == null) {
-                    logger.log("DHBNJPA0003", new Object[] { entry.getPath() });
-                } else {
-                    logger.log("DHBNJPA0004", new Object[] { entry.getPath(),
-                            unitName });
-                }
-            }
-            addMappingFileStream(unitName, entry.getInputStream());
-        }
-    }
-
-    public void setPersistenceClassAutoDetector(
-            final ClassAutoDetector[] detectors) {
-        for (final ClassAutoDetector detector : detectors) {
-            addPersistenceClassAutoDetector(detector);
-        }
-    }
-
-    public void addPersistenceClassAutoDetector(final ClassAutoDetector detector) {
-        addPersistenceClassAutoDetector(null, detector);
-    }
-
-    public void addPersistenceClassAutoDetector(final String unitName,
-            final ClassAutoDetector detector) {
-        autoDetection = true;
-
-        for (final Class clazz : detector.detect()) {
-            if (logger.isDebugEnabled()) {
-                if (unitName == null) {
-                    logger.log("DHBNJPA0001", new Object[] { clazz.getName() });
-                } else {
-                    logger.log("DHBNJPA0002", new Object[] { clazz.getName(),
-                            unitName });
-                }
-            }
-            addPersistenceClass(unitName, clazz);
-        }
-    }
-
-    public List<String> getMappingFiles() {
-        return getMappingFiles(null);
-    }
-
-    public List<String> getMappingFiles(final String unitName) {
-        if (mappingFiles.containsKey(unitName)) {
-            return mappingFiles.get(unitName);
-        }
-        return Collections.emptyList();
-    }
-
-    public List<InputStream> getMappingFileStreams() {
-        return getMappingFileStreams(null);
-    }
-
-    public List<InputStream> getMappingFileStreams(final String unitName) {
-        if (mappingFileStreams.containsKey(unitName)) {
-            return mappingFileStreams.get(unitName);
-        }
-        return Collections.emptyList();
-    }
-
-    public List<Class<?>> getPersistenceClasses() {
-        return getPersistenceClasses(null);
-    }
-
-    public List<Class<?>> getPersistenceClasses(final String unitName) {
-        if (persistenceClasses.containsKey(unitName)) {
-            return persistenceClasses.get(unitName);
-        }
-        return Collections.emptyList();
-    }
-
-    public boolean isAutoDetection() {
-        return autoDetection;
-    }
 }
