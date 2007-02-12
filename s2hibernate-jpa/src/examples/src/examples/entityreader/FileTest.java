f53e63aab7ee424e3508f093b1a1106f94c04719
nakamura-to <toshihiro.nakamura@gmail.com>
2007-02-12 13:09:42 +0000

    エンティティをDataSetと比較するテストケースのサンプルを追加しました。
    git-svn-id: https://www.seasar.org/svn/s2hibernate/trunk@179 d0420757-6e02-0410-97e1-b335d905b555

---
+++ s2hibernate-jpa/src/examples/src/examples/entityreader/FileTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.entityreader;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.junit.runner.RunWith;
+import org.seasar.framework.unit.Seasar2;
+import org.seasar.framework.unit.TestContext;
+import org.seasar.hibernate.jpa.S2HibernateConfiguration;
+
+import static org.seasar.framework.unit.S2Assert.*;
+
+/**
+ * 
+ * @author nakamura
+ */
+@RunWith(Seasar2.class)
+public class FileTest {
+
+    private TestContext context;
+
+    private EntityManager em;
+
+    public void before() throws Exception {
+        context.include("jpa.dicon");
+        S2HibernateConfiguration cfg = context
+                .getComponent(S2HibernateConfiguration.class);
+        cfg.addPersistenceClass(File.class);
+        cfg.addPersistenceClass(Document.class);
+        cfg.addPersistenceClass(Folder.class);
+    }
+
+    public void polymorphicQuery() throws Exception {
+        Folder root = new Folder();
+        root.setName("root");
+        Folder folder = new Folder();
+        folder.setName("folder");
+        folder.setParent(root);
+        Document document = new Document();
+        document.setName("document");
+        document.setSize(100);
+        document.setParent(folder);
+        folder.getChildren().add(document);
+
+        em.persist(root);
+        em.persist(folder);
+        em.persist(document);
+
+        Query query = em.createQuery("SELECT f FROM File f ORDER BY f.name");
+        assertEntityEquals(context.getExpected(), query.getResultList());
+    }
+}
